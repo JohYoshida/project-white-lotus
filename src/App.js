57a2c1e2bb2744fab8583796c604b202edb5b66e
Joh Yoshida <johyoshida@gmail.com>
2017-12-14 12:50:40 -0800

    Dynamic routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Battle from './Battle.jsx';
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.socket = new WebSocket('ws://localhost:3001');
-    this.state = {
-
-    };
+    this.state = {};
   }
 
   componentDidMount() {
-    let websocket = this.socket
+    let websocket = this.socket;
     websocket.addEventListener('open', event => {
       websocket.send('Hello!?');
     });
@@ -21,17 +18,65 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Routes />
+    )
   }
 }
+
+const Routes = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Monsters</Link></li>
+        <li><Link to="/store">Store</Link></li>
+        <li><Link to="/teams">Teams</Link></li>
+        <li><Link to="/battle">Battle</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Monsters}/>
+      <Route path="/store" component={Store}/>
+      <Route path="/teams" component={Teams}/>
+      <Route path="/battle" component={Battle}/>
+    </div>
+  </Router>
+)
+
+const Monsters = ({ match }) => (
+  <div>
+    <h2>Monsters</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/monster`}>
+          Monster
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Monster}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a Monster.</h3>
+    )}/>
+  </div>
+)
+
+const Monster = ({ match }) => (
+  <div>
+    <h3>{match.params.monsterId}</h3>
+  </div>
+)
+
+const Store = () => (
+  <div>
+    <h2>Store</h2>
+  </div>
+)
+
+const Teams = () => (
+  <div>
+    <h2>Teams</h2>
+  </div>
+)
 
 export default App;
