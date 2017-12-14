297ac3970871b1610e73fd52239d6b30a1c388d6
Quinlan Jager <quinlan.jager@gmail.com>
2017-12-14 11:25:54 -0800

    finalizing seeds and databse.

--- lotus_server/db/migrations/20171213231341_initial_tables.js
+++ lotus_server/db/migrations/20171213231341_initial_tables.js
@@ -24,7 +24,6 @@ exports.up = function(knex, Promise) {
       table.increments();
       table.string('image_url');
       table.integer('hp');
-      table.integer('current_hp');
       table.integer('accuracy_bonus');
       table.integer('type_id').references('types');
       table.string('creature');
@@ -56,7 +55,6 @@ exports.up = function(knex, Promise) {
       table.string('name');
       table.boolean('aoe');
       table.boolean('dot');
-      table.integer('type_id').references('types');
     })
   ]);
 };
