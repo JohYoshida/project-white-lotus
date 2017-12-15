67fe178b3d97fe6533a6cec1067bdd688b0163d4
Joh Yoshida <johyoshida@gmail.com>
2017-12-14 17:38:32 -0800

    Get everything from monsters table so React can fetch it

--- lotus_server/server.js
+++ lotus_server/server.js
@@ -1,12 +1,27 @@
 'uses strict';
 
+require('dotenv').config();
 const express = require('express')
 const WebSocket = require('ws');
 const SocketServer = WebSocket.Server;
+const server = express();
+
+const dbconfig = require('./knexfile.js')[process.env.DB_ENV];
+const knex = require('knex')(dbconfig);
 
 const PORT = 3001;
 
-const server = express().listen(PORT, '0.0.0.0', 'localhost', () => {
+server.get('/monsters', (req, res) => {
+  knex.from('monsters')
+  .then(monsters => {
+    for (let monster in monsters) {
+      console.log(monster, monsters[monster]);
+    }
+    res.json(monsters);
+  });
+});
+
+server.listen(PORT, '0.0.0.0', 'localhost', () => {
   console.log(`Listening on ${PORT}`);
 });
 
