0cdcf50a71d5620a6f5eae06dc9c0692d8e0fa43
dbaylyuvic <dbayly@uvic.ca>
2017-12-19 15:19:10 -0800

    Added migration and new seed. User ids are now uuids and monsters have a image column

--- lotus_server/db/seeds/add_users.js
+++ lotus_server/db/seeds/add_users.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const uuidv1 = require('uuid/v1');
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('users').del()
@@ -6,13 +7,13 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('users').insert([
         {
-          id: 1,
+          id: uuidv1(),
           email: 'admin',
           password: bcrypt.hashSync('pass',10),
           brouzoff:9999
        },
        {
-          id: 2,
+          id: uuidv1(),
           email: 'user',
           password: bcrypt.hashSync('pass',10),
           brouzoff:100
--- lotus_server/lib/add_user.js
+++ lotus_server/lib/add_user.js
@@ -1,10 +1,11 @@
 const dbconfig = require('../knexfile.js')[process.env.DB_ENV];
 const knex = require('knex')(dbconfig);
 const bcrypt = require('bcrypt');
+const uuidv1 = require('uuid/v1');
 const add_user = (res,email,password) => {
-  knex('users').insert({'email':email,'password':bcrypt.hashSync(password,10) ,brouzoff:100}).then( result =>{
+  knex('users').insert({'id':uuidv1(),'email':email,'password':bcrypt.hashSync(password,10) ,brouzoff:100}).then( result =>{
     console.log(result);
-     knex.table('users').first('id','email','brouzoff','password').where('email',email) .then(ids => {
+     knex.table('users').first('id','email','brouzoff').where('email',email) .then(ids => {
       if(ids!== undefined){
         res.send(ids);
       }else{
