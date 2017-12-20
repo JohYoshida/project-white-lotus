570501fc21869f3f3456cc999e4f9b755a07f622
Joh Yoshida <johyoshida@gmail.com>
2017-12-20 15:55:47 -0800

    Get user's brouzoff

--- lotus_server/server.js
+++ lotus_server/server.js
@@ -19,17 +19,20 @@ const generateUser = require('./lib/generate_user');
 
 // Routes
 const socketRouter = require('./routes/battles_routes')(server);
+const monsterRouter = require('./routes/monster_routes')(knex);
+
 // Functions
 const buildMonstersJSON = require('./lib/build_monsters_json');
 const buildMonsterJSON = require('./lib/build_monster_json');
 const generatePlayer = require('./lib/generate_player');
-const addUser = require('./lib/add_user')
-const monsterRouter = require('./routes/monster_routes')(knex);
+const addUser = require('./lib/add_user');
 
 // Body Parser
 server.use(bodyParser.urlencoded({ extended: false }));
 // This is required to parse POST fetch requests for the store
 server.use(bodyParser.json());
+
+// Cookie Parser
 server.use(cookieParser())
 
 // Default room for testing.
@@ -42,9 +45,18 @@ server.post('/login', (req, res) => {
   // find a user by email
   generateUser(res, req.body.email, req.body.password);
 });
+
 server.post('/users', (req, res) => {
   // find a user by email
-  addUser(res, req.params.email,req.params.password);
+  addUser(res, req.body.email, req.body.password);
+});
+
+server.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  knex('users').first('brouzoff').where('id', id)
+    .then(brouzoff => {
+      res.send(JSON.stringify(brouzoff));
+    });
 });
 
 // Start server
