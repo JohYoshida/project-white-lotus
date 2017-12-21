2bd027f5f57514e047e88c80ccc917612c05fc32
Joh Yoshida <johyoshida@gmail.com>
2017-12-20 16:12:22 -0800

    Change starting brouzoff to 200

--- lotus_server/lib/add_user.js
+++ lotus_server/lib/add_user.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const uuidv1 = require('uuid/v1');
 const add_user = (res, email, password) => {
   bcrypt.hash(password, 10, (err, hash) => {
-    knex('users').insert({'id':uuidv1(), 'email':email, 'password':hash, brouzoff:100}, 'id').then(id =>{
+    knex('users').insert({'id': uuidv1(), 'email': email, 'password': hash, brouzoff: 200}, 'id').then(id =>{
       console.log('Created user with id#', id);
       knex('users').select('id','email','brouzoff','password').where('id', id[0]) .then(user => {
         res.send(JSON.stringify(user[0]));
