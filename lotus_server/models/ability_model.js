7159deea30688b6f57dedc20d78d5513249eebd3
Joh Yoshida <johyoshida@gmail.com>
2017-12-21 23:15:26 -0800

    Added functions for each ability. TODO: actually implement the effects

--- lotus_server/models/ability_model.js
+++ lotus_server/models/ability_model.js
@@ -5,7 +5,53 @@ const Ability = bookshelf.Model.extend({
 });
 
 const abilityFuncs = {
-  avoid: function(player){
+  // TODO: Implement these with the proper effects
+  supercharge: function(player){
+    if (!player.activeMonster) {
+      return;
+    }
+    player.activeMonster.hp += 1;
+    // this is bound to the monster who has this ability, this ability is one time use. Increases max hp.
+    this.passiveActive = false;
+    return `${this.name} increased ${player.activeMonster.name}'s HP by 1`;
+  },
+  nanomachine_swarm: function(player){
+    if (!player.activeMonster) {
+      return;
+    }
+    player.activeMonster.hp += 1;
+    // this is bound to the monster who has this ability, this ability is one time use. Increases max hp.
+    this.passiveActive = false;
+    return `${this.name} increased ${player.activeMonster.name}'s HP by 1`;
+  },
+  electric_shield: function(player){
+    if (!player.activeMonster) {
+      return;
+    }
+    player.activeMonster.hp += 1;
+    // this is bound to the monster who has this ability, this ability is one time use. Increases max hp.
+    this.passiveActive = false;
+    return `${this.name} increased ${player.activeMonster.name}'s HP by 1`;
+  },
+  pierce: function(player){
+    if (!player.activeMonster) {
+      return;
+    }
+    player.activeMonster.hp += 1;
+    // this is bound to the monster who has this ability, this ability is one time use. Increases max hp.
+    this.passiveActive = false;
+    return `${this.name} increased ${player.activeMonster.name}'s HP by 1`;
+  },
+  crush: function(player){
+    if (!player.activeMonster) {
+      return;
+    }
+    player.activeMonster.hp += 1;
+    // this is bound to the monster who has this ability, this ability is one time use. Increases max hp.
+    this.passiveActive = false;
+    return `${this.name} increased ${player.activeMonster.name}'s HP by 1`;
+  },
+  spray: function(player){
     if (!player.activeMonster) {
       return;
     }
