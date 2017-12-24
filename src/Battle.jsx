4edcb8c63563a374ccb1854f2705554d1fef8490
Quinlan Jager <quinlan.jager@gmail.com>
2017-12-24 11:45:04 -0800

    fixed up some battle styling.

--- src/Battle.jsx
+++ src/Battle.jsx
@@ -10,43 +10,39 @@ import editBrouzoff from './lib/editBrouzoff.js';
 class Battle extends Component {
   constructor(props) {
     super(props);
-    this.state = {ready: false, game:{}, messages: [], player:{}, opponent:{}};
+    this.state = {ready:false, game:{}, messages: [], player:{}, opponent:{}};
     this.joinGame = this.joinGame.bind(this);
-
   }
   // Handles sending join game requests.
   joinGame(event){
     this.state.ready || this.setState({ready:true});
-    this.setState({battlerId:uuid()})
+    this.setState({battlerId:uuid()});
     const button = event.target;
     // build the WebSocket.
-    this.socket = generateBattleSocket(this)
+    this.socket = generateBattleSocket(this);
   }
 
   isWinner(){
     const {gameOver} = this.state.game;
     if(gameOver.winner.id === this.state.id){
-      editBrouzoff(this.state.game, 500)
+      editBrouzoff(this.state.game, 500);
       return (<p>You won!</p>);
     }
-    editBrouzoff(this.state.game, 250)
+    editBrouzoff(this.state.game, 250);
     return(<p>You lost!</p>);
   }
 
   render() {
     return (
-      <main>
+      <main id="battlefield">
         {this.state.game.gameOver && <Modal header="Game over" mainContent={this.isWinner()} footer={<a className="button" href="/">Done</a>} />}
-        {!this.state.ready && <button onClick={this.joinGame}>1</button>}
-        {!this.state.ready && <button onClick={this.joinGame}>2</button>}
-        {/* <h2>Opponent {this.state.game.idlePlayer && this.state.game.idlePlayer.id}</h2> */}
+        <button onClick={this.joinGame}>2</button>
+        <p>{this.state.game.idlePlayer && this.state.game.idlePlayer.id}</p>
         {this.state.ready && <Opponent className='opponent' player={this.state.opponent} curUserId={this.state.id} /> }
-
-        {/* <h2>Player {this.state.game.activePlayer && this.state.game.activePlayer.id}</h2> */}
+        <p>{this.state.game.activePlayer && this.state.game.activePlayer.id}</p>
         {this.state.ready && <Player className='player' player={this.state.player} socket={this.socket} curUserId={this.state.id} />}
 
-        {/* <h3>Messages</h3> */}
-        <MessageBox className='message-box' messages={this.state.messages} />
+        {this.state.ready && <MessageBox className='message-box' messages={this.state.messages} />}
       </main>
     );
   }
