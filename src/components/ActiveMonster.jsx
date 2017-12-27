735b1f727595628679ba60746c8fba181374b258
Quinlan Jager <quinlan.jager@gmail.com>
2017-12-26 23:39:26 -0800

    further styling, adding media queries.

--- src/components/ActiveMonster.jsx
+++ src/components/ActiveMonster.jsx
@@ -11,7 +11,7 @@ class ActiveMonster extends Component {
     const attacks = [];
     for(let attackName in monster.attacks){
       const attack = monster.attacks[attackName];
-      attacks.push(<button disabled={!this.props.player.turn} className='button' key={attack.id} onClick={sendAttack} data-name={attack.name}>{attack.name}</button>);
+      attacks.push(<span disabled={!this.props.player.turn} className='sword-ico' key={attack.id} onClick={sendAttack} data-name={attack.name}></span>);
     }
     return (
       <section className="Attacks">
