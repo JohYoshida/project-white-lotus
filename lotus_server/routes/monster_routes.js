dddf8274c92ba9fc5f303a94c3e72894c9f6ee0a
Quinlan Jager <quinlan.jager@gmail.com>
2018-01-08 15:22:34 -0800

    If a monster is deleted, teams they are associated with are also deleted.

--- lotus_server/routes/monster_routes.js
+++ lotus_server/routes/monster_routes.js
@@ -4,6 +4,7 @@ const buildMonsterJSON = require('../lib/build_monster_json');
 const express = require('express');
 const getCreature = require('../lib/generate_monster.js');
 const Monster = require('../models/monster_model.js')();
+const {Team, TeamMonster} = require('../models/team_model.js');
 
 module.exports = (db) => {
   const User = require('../models/user_model')(db);
@@ -20,13 +21,25 @@ module.exports = (db) => {
     const userId = req.session.id;
     const {id} = req.params;
     new Monster({id}).fetch().then(monster => {
-      if(monster.get('user_id') !== userId){
-        res.status(400).send();
+      if(monster.get('user_id') !== userId || !userId){
+        res.status(400).send(JSON.stringify({error: 'Not authorized to complete this task.'}));
         return;
       }
-      monster.destroy().then(() => {
-        res.send(JSON.stringify({flash: `Monster ${id} has been deleted.`}));
-      })
+      // grab the rows from the many-to-many table put them into a collection
+      new TeamMonster().where({monster_id: monster.get('id')}).fetchAll().then(TeamMonsterCollection => {
+        const deletionPromises = [];
+        // collect all deletion requests (where: clause necessary since the many-to-many table has no id attribute)
+        TeamMonsterCollection.forEach(model => {
+          deletionPromises.push(new Team().where({id: model.get('team_id')}).destroy());
+          deletionPromises.push(model.where({id: model.get('team_id')}).destroy());
+        });
+        Promise.all(deletionPromises).then(() => {
+          // after teams have been deleted, delete the monster and send a confirmation message.
+          monster.destroy().then(() => {
+            res.status(200).send(JSON.stringify({flash: `Monster ${id} has been deleted.`}));
+          });
+        });
+      });
     });
   });
 
